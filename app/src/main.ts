b8e7cb1c37f5239626a34a09578eb71e89a691c4
thiagoMoraes21 <thiago4x70@gmail.com>
2021-08-02 19:05:38 -0300

    add back the http connection on main, and configured a tcp connection on the math service

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,19 +1,14 @@
-import { ClientOptions, ClientProxyFactory, Transport } from "@nestjs/microservices";
 import { Logger } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 const logger = new Logger('Main');
 
-const microserviceOptions: ClientOptions = {
-    transport: Transport.TCP,
-    options: {
-        host: '127.0.0.1',
-        port: 8877,
-    }
-};
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    logger.log('Client is listening...');
+}
+bootstrap();
 
-const client = ClientProxyFactory.create(microserviceOptions);
 
-// .send<ReturnType, ParamType>(pattern, param)
-client
-    .send<number, number[]>('add', [1, 2, 3]) 
-    .subscribe(result => logger.log(result));
--- app/src/math.service.ts
+++ app/src/math.service.ts
@@ -1,9 +1,28 @@
+import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { Logger } from "@nestjs/common";
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MathService {
+    private client: ClientProxy;
+    private logger = new Logger('MathService');
 
-    public accumulate(data: number[]): number {
-        return (data || []).reduce((a, b) => Number(a) + Number(b));
+    constructor() {
+        this.client = ClientProxyFactory.create({
+            transport: Transport.TCP,
+            options: {
+                host: '127.0.0.1',
+                port: 8877,
+            }
+        });
+        this.logger.log('connected...');
+    }
+
+    public accumulate(data: number[]): Observable<number> {
+        this.logger.log('[accumulate] - ' + data);
+
+        // .send<ReturnType, ParamType>(pattern, param)
+        return this.client.send<number, number[]>('add', data);
     }
 }
