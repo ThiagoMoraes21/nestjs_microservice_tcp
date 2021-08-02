3290421d4ef82e3c94685d459e8ceb3e552733fb
thiagoMoraes21 <thiago4x70@gmail.com>
2021-08-02 18:46:29 -0300

    configure the main app to be a microservice tcp client

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ClientOptions, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import { Logger } from "@nestjs/common";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const logger = new Logger('Main');
+
+const microserviceOptions: ClientOptions = {
+    transport: Transport.TCP,
+    options: {
+        host: '127.0.0.1',
+        port: 8877,
+    }
+};
+
+const client = ClientProxyFactory.create(microserviceOptions);
+
+// .send<ReturnType, ParamType>(pattern, param)
+client
+    .send<number, number[]>('add', [1, 2, 3]) 
+    .subscribe(result => logger.log(result));
